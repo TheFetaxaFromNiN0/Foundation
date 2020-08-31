c796fd9d4149605cb89d49e67ce52c35fdf62d6e
Mihail <mikhail.golubev@firstlinesoftware.com>
2020-08-31 09:04:47 +0300

    fix

--- src/Foundation/Features/Blocks/AdsBlockController.cs
+++ src/Foundation/Features/Blocks/AdsBlockController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using EPiServer.Web.Mvc;
+using Foundation.Cms.Blocks;
 
 namespace Foundation.Features.Blocks
 {
-    public class AdsBlockController
+    public class AdsBlockController: BlockController<AdsBlock>
     {
+        public override ActionResult Index(AdsBlock currentBlock)
+        {
+            return PartialView("~/Features/Blocks/Views/AdsBlock.cshtml", currentBlock);
+        }
     }
 }
